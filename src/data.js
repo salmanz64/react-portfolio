7d4f5af590e3dcf5d2b809ba988145c66c0a922b
Salman Noushad <salmannoushad003@gmail.com>
2024-11-06 22:59:52 +0530

    portfolio

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "He's to good in using Linux.",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
+    name: "Linus Torvalds",
+    company: "NSA",
+  },
+  {
+    quote:
+      "He contributes very good to FOSS on Github.",
+    image: "https://randomuser.me/api/portraits/men/93.jpg",
+    name: "Richard Stallman",
+    company: "FSF",
+  },
+];
+
+export const skills = [
+  "Web development",
+  "Flutter",
+  "C",
+  "java",
+  "Git",
+  "Sql",
+];
