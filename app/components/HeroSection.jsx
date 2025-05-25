142b152db5e2ba291fcb3b93377be746fc68dcd2
Salman T <salmannoushad003@gmail.com>
2025-05-25 23:52:22 +0530

    updates on portfolio

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,18 +1,48 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
+
+import { TypeAnimation } from 'react-type-animation';
+
+
+    
+ 
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
           <div className="col-span-7 place-self-center" >
-              <h1 className='text-white mb-4 text-4xl lg:text-6xl  font-extrabold'>Hello , I'm Salman</h1>
-                  <p className='text-[#ADB7BE] mb-4 text-lg lg:text-xl'>
+              <h1 className='text-white mb-4 text-4xl lg:text-6xl  font-extrabold'>
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello , I'm {" "}</span>
+              <br></br>
+              <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Salman T',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Web Developer',
+        1000,
+        'Mobile Developer',
+        1000,
+        
+      ]}
+      wrapper="span"
+      speed={50}
+      
+      repeat={Infinity}
+    />
+              
+              </h1>
+                  <p className='text-[#ADB7BE] text-base mb-6 sm-text-lg lg:text-xl'>
                 afhfshffhsafkjshfhjshj hfasjfsaj afsdfjkasfh sjfjhasfjksjkfh sfhjashfjkashfj
                 fasjkfshfjsh.
                   </p>
-                  <button className='rounded-full px-6 py-4 bg-white hover:bg-slate-200 text-black ml-4'>Hire Me</button>
-                  <button className='rounded-full px-6 py-4 bg-transparent border border-white hover:bg-slate-800 ml-4'>Download CV</button>
+                  <button className='rounded-full w-full sm:w-fit px-6 py-3 mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white hover:bg-slate-200 text-white ml-4'>Hire Me</button>
+                  <button className='rounded-full  w-full sm:w-fit px-1 py-1 mr-4 bg-transparent border bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-800 ml-4 mt-4'>
+                  <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'> Download CV</span>
+                 </button>
           </div>
           <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
             <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
 
 export default function Home() {
   return <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto" >
+    <Navbar/>
     <HeroSection />
   </main>
   
